18f9d3f0ae739a4bc21087360ffa91335f6c6c21
tatyana-shestakova <tatyana_sch@list.ru>
2020-11-10 02:36:04 +0300

    Правки в модуле

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,14 @@
 'use strict';
 
-window.dialog.setup.querySelector('.setup-similar').classList.remove('hidden');
+(function () {
+  window.dialog.setup.querySelector('.setup-similar').classList.remove('hidden');
 
-const coatColorUser = document.querySelector('.setup-wizard .wizard-coat');
-const eyesColorUser = document.querySelector('.setup-wizard .wizard-eyes');
-const fireballColorUser = document.querySelector('.setup-fireball-wrap');
+  const coatColorUser = document.querySelector('.setup-wizard .wizard-coat');
+  const eyesColorUser = document.querySelector('.setup-wizard .wizard-eyes');
+  const fireballColorUser = document.querySelector('.setup-fireball-wrap');
 
-window.changeColor(coatColorUser, window.util.wizardCoats, '.setup-player input[name="coat-color"]');
-window.changeColor(eyesColorUser, window.util.wizardEyes, '.setup-player input[name="eyes-color"]');
-window.changeColor(fireballColorUser, window.util.fireballsColor, '.setup-player input[name="fireball-color"]');
+  window.changeColor(coatColorUser, window.util.wizardCoats, '.setup-player input[name="coat-color"]');
+  window.changeColor(eyesColorUser, window.util.wizardEyes, '.setup-player input[name="eyes-color"]');
+  window.changeColor(fireballColorUser, window.util.fireballsColor, '.setup-player input[name="fireball-color"]');
 
+})();
